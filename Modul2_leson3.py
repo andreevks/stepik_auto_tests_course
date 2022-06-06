ede5a2a7b1bff083b25fbe96f2db4c7ba6363e36
andreevks <andreevks@mail.ru>
2022-06-06 14:24:32 +0300

    Modul2_leson3.py added

--- Modul2_leson3.py
+++ Modul2_leson3.py
@@ -1,24 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def calc(x):
-    return math.log(abs(12*math.sin(int(x))))
-
-EXE_PATH = 'D:\Рабочий стол\Selenium_project\chromedriver\chromedriver.exe'
 url = 'http://suninjuly.github.io/explicit_wait2.html'
 
 try:
-    browser = webdriver.Chrome(executable_path=EXE_PATH)
+    browser = webdriver.Chrome()
     browser.get(url)
-   #price = browser.find_element(By.ID, "price")
     WebDriverWait(browser, 15).until(EC.text_to_be_present_in_element((By.ID, "price"), '$100'))
     browser.find_element(By.ID, "book").click()
-    # browser.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-    # browser.switch_to.window(browser.window_handles[1])
     x = browser.find_element(By.CSS_SELECTOR, '#input_value').text
     browser.find_element(By.CSS_SELECTOR, '#answer').send_keys(calc(x))
     browser.find_element(By.CSS_SELECTOR, '#solve').click()
